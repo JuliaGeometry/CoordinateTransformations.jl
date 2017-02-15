62ba22e0d9e34e0c1b3e2aae5c54cd98c7fa7576
Andy Ferris <andy.ferris@roames.com.au>
2017-02-16 09:57:47 +1000

    More precision for Julia versions

--- test/perspective.jl
+++ test/perspective.jl
@@ -1,7 +1,7 @@
 @testset "Perspective transformation" begin
     @test PerspectiveMap()([2.0, -1.0, 0.5]) ≈ [4.0, -2.0]
 
-    if VERSION > v"0.6-"
+    if VERSION >= v"0.6.0-dev.948"
         @test cameramap(2.0)                  ≈ LinearMap(UniformScaling(2.0)) ∘ PerspectiveMap()
         @test cameramap((1.1,2.2))            ≈ LinearMap([1.1 0; 0 2.2]) ∘ PerspectiveMap()
         @test cameramap(1.1,       (3.3,4.4)) ≈ Translation([-3.3,-4.4]) ∘ LinearMap(UniformScaling(1.1)) ∘ PerspectiveMap()
