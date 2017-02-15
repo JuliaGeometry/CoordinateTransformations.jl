fc8f2e77137fc001f81270f70918c422ead2933e
Andy Ferris <andy.ferris@roames.com.au>
2017-02-15 10:00:04 +1000

    Changed tests to avoid defining isapprox on UniformScaling

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,6 @@ using Base.Test
 using ForwardDiff: Dual, partials
 using StaticArrays
 
-# See https://github.com/JuliaLang/julia/issues/18858
-Base.isapprox(a::UniformScaling, b::UniformScaling; kwargs...) = isapprox(a.λ, b.λ; kwargs...)
-
 @testset "CoordinateTransformations" begin
 
     include("core.jl")
