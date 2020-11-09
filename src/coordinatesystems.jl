291a22f41332764b5c024e06b618570cf55e8b67
Jan Weidner <jw3126@gmail.com>
2020-11-09 04:34:05 +0100

    improve Spherical docs (#68)
    Co-authored-by: Waldir Pimenta <waldyrious@gmail.com>

--- src/coordinatesystems.jl
+++ src/coordinatesystems.jl
@@ -73,6 +73,25 @@ Base.convert(::Type{Polar}, v::AbstractVector) = PolarFromCartesian()(v)
 #############################
 """
 Spherical(r, θ, ϕ) - 3D spherical coordinates
+
+There are many Spherical coordinate conventions and this library uses a somewhat exotic one.
+Given a vector `v` with Cartesian coordinates `xyz`, let `v_xy = [x,y,0]` be the orthogonal projection of `v` on the `xy` plane.
+
+* `r` is the radius. It is given by `norm(v, 2)`.
+* `θ` is the azimuth. It is the angle from the x-axis to `v_xy`
+* `ϕ` is the latitude. It is the angle from `v_xy` to `v`.
+
+```jldoctest
+julia> using CoordinateTransformations
+
+julia> v = randn(3);
+
+julia> sph = SphericalFromCartesian()(v);
+
+julia> r = sph.r; θ=sph.θ; ϕ=sph.ϕ;
+
+julia> v ≈ [r * cos(θ) * cos(ϕ), r * sin(θ) * cos(ϕ), r*sin(ϕ)]
+true
 """
 struct Spherical{T,A}
     r::T
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,12 @@ using CoordinateTransformations
 using ForwardDiff: Dual, partials
 using StaticArrays
 using Unitful
+using Documenter: doctest
+
 
 @testset "CoordinateTransformations" begin
 
+    doctest(CoordinateTransformations)
     include("core.jl")
     include("coordinatesystems.jl")
     include("affine.jl")
