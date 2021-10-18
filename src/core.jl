ad98845b8c272dd86d4facbeb4ed7deaa80635e9
Vedant Puri <vedantpuri@gmail.com>
2021-10-18 07:56:18 -0500

    typo
    `Transformation` is an abstract type (ref line 15 in core.jl). `AbstractTransformation` does not exist

--- src/core.jl
+++ src/core.jl
@@ -119,7 +119,7 @@ end
 
 
 """
-    transform_deriv_params(trans::AbstractTransformation, x)
+    transform_deriv_params(trans::Transformation, x)
 
 A matrix describing how differentials on the parameters of `trans` flow through
 to the output of transformation `trans` given input `x`.
