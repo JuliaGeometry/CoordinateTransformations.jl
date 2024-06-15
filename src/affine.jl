15270616ed51d416fc801d598edb5357a5e40b63
Abhro R <5664668+abhro@users.noreply.github.com>
2024-06-15 16:17:30 -0400

    Add docstring for AbstractAffineMap

--- src/affine.jl
+++ src/affine.jl
@@ -1,3 +1,9 @@
+"""
+    abstract type AbstractAffineMap <: Transformation
+
+`AbstractAffineMap` is the supertype of all affine transformations,
+including linear transformations.
+"""
 abstract type AbstractAffineMap <: Transformation end
 
 """
